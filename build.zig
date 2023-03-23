54c96a8b3a4346b311185344b55d903d3fd85d81
Felix "xq" Queißner <xq@random-projects.net>
2023-03-23 08:34:14 +0100

    Update to 0.11.0-dev.2227+f9b582950, adds module support

--- build.zig
+++ build.zig
@@ -1,12 +1,18 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
+    _ = b.addModule("any-pointer", .{
+        .source_file = .{ .path = "any-pointer.zig" },
+    });
+
     const test_step = b.step("test", "Run library tests");
 
-    const all_modes = std.enums.values(std.builtin.Mode);
-    for (all_modes) |mode| {
-        const main_tests = b.addTest("tests.zig");
-        main_tests.setBuildMode(mode);
+    const all_modes = std.enums.values(std.builtin.OptimizeMode);
+    for (all_modes) |optimize| {
+        const main_tests = b.addTest(.{
+            .root_source_file = .{ .path = "tests.zig" },
+            .optimize = optimize,
+        });
 
         test_step.dependOn(&main_tests.step);
     }
